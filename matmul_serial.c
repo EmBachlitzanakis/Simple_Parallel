05f2121001e05ec824255195cc4a5112ba59eaea
EmBachlitzanakis <EmBachlitzanakis@gmail.com>
2022-10-15 12:39:00 +0300

    matmul_parrallel_mpi.c

---
+++ matmul_serial.c
@@ -0,0 +1,84 @@
+/* Serial program for matrix-matrix product.
+ *
+ * VVD
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 256
+int A[N][N], B[N][N], C[N][N];
+int readmat(char *fname, int *mat, int n), 
+    writemat(char *fname, int *mat, int n);
+
+int main()
+{
+	int i, j, k, sum;
+
+	/* Read matrices from files: "A_file", "B_file" 
+	 */
+	if (readmat("Amat256.txt", (int *) A, N) < 0) 
+		exit( 1 + printf("file problem\n") );
+	if (readmat("Bmat256.txt", (int *) B, N) < 0) 
+		exit( 1 + printf("file problem\n") );
+	clock_t tic = clock();
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+		{
+			for (k = sum = 0; k < N; k++)
+				sum += A[i][k]*B[k][j];
+			C[i][j] = sum;
+		};
+	
+
+	/* Save result in "Cmat1024"
+	 */
+	
+
+    	
+	writemat("Cmat1024", (int *) C, N);
+	clock_t toc = clock();
+	printf("Elapsed: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+	return (0);
+}
+
+
+/* Utilities to read & write matrices from/to files
+ * VVD
+ */
+
+#define _mat(i,j) (mat[(i)*n + (j)])
+
+
+int readmat(char *fname, int *mat, int n)
+{
+	FILE *fp;
+	int  i, j;
+	
+	if ((fp = fopen(fname, "r")) == NULL)
+		return (-1);
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			if (fscanf(fp, "%d", &_mat(i,j)) == EOF)
+			{
+				fclose(fp);
+				return (-1); 
+			};
+	fclose(fp);
+	return (0);
+}
+
+
+int writemat(char *fname, int *mat, int n)
+{
+	FILE *fp;
+	int  i, j;
+	
+	if ((fp = fopen(fname, "w")) == NULL)
+		return (-1);
+	for (i = 0; i < n; i++, fprintf(fp, "\n"))
+		for (j = 0; j < n; j++)
+			fprintf(fp, " %d", _mat(i, j));
+	fclose(fp);
+	return (0);
+}
